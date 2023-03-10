c1e565b96c1f569c313f05a0c70971468fdfd855
fustom <fustom@gmail.com>
2023-03-10 14:24:07 +0100

    Update sensor

--- custom_components/balaton_water_temperature/sensor.py
+++ custom_components/balaton_water_temperature/sensor.py
@@ -1,43 +1,72 @@
 """Support for getting the Balatonlelle water temperature."""
 from __future__ import annotations
 
+from datetime import timedelta
+from typing import Final
+
 import logging
 import aiohttp
 import re
 import ast
+import voluptuous as vol
 
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (
+    PLATFORM_SCHEMA as BASE_PLATFORM_SCHEMA,
     SensorDeviceClass,
     SensorEntity,
     SensorStateClass,
 )
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import CONF_NAME, UnitOfTemperature
+from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
 PATH = "https://www.idokep.hu/terkep/hu/vizho.js"
 REFERER = "https://www.idokep.hu/hoterkep/balaton"
-PLACE = "Balatonlelle"
+DEFAULT_NAME = "Balatonlelle"
+
+PLATFORM_SCHEMA: Final = BASE_PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    }
+)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensor platform."""
-    async_add_entities([BalatonWaterTemperature(PLACE)])
+    name = config[CONF_NAME]
+    async_add_entities([BalatonWaterTemperature(name)])
 
 
 class BalatonWaterTemperature(SensorEntity):
     """Representation of a sensor."""
 
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_native_unit_of_measurement = TEMP_CELSIUS
-    _attr_state_class = SensorStateClass.MEASUREMENT
-
-    def __init__(self, place):
+    def __init__(self, place) -> None:
         """Initialize a sensor."""
         self.place = place
-        self._attr_name = f"{self.place} water temperature"
-        self._attr_unique_id = f"{self.place}WaterTemperature"
 
+    @property
+    def unique_id(self) -> str | None:
+        return f"{self.place}WaterTemperature"
+
+    @property
+    def name(self) -> str | None:
+        return f"{self.place} water temperature"
+
+    @property
+    def device_class(self) -> SensorDeviceClass | None:
+        return SensorDeviceClass.TEMPERATURE
+
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        return UnitOfTemperature.CELSIUS
+
+    @property
+    def state_class(self) -> SensorStateClass | str | None:
+        return SensorStateClass.MEASUREMENT
+
+    @Throttle(timedelta(minutes=5))
     async def async_update(self):
         """Get the latest data."""
         self._attr_native_value = await self.__async_request()
@@ -45,7 +74,7 @@ class BalatonWaterTemperature(SensorEntity):
     async def __async_request(
         self,
         path: str = PATH,
-        method: str = "GET",
+        method: str = aiohttp.hdrs.METH_GET,
     ) -> int:
         """Async request with aiohttp"""
         headers = {"referer": REFERER}
